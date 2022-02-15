b0a7d1aef5c9204867b73a0717d88d4c08f4ee57
Faceless Creator <theunnamedh@gmail.com>
2022-02-15 18:38:39 +0300

    1.1.2 - membomb rewrited from malloc to mmap

--- src/membomb.cpp
+++ src/membomb.cpp
@@ -1,7 +1,7 @@
 #include <chrono>
 #include <thread>
 #include <unistd.h>
-#include <malloc.h>
+#include <sys/mman.h>
 #include <string.h>
 
 #define TIME_BETWEEN_ALLOCATIONS 50
@@ -11,7 +11,7 @@ int main() {
 
     while(1) {
         std::this_thread::sleep_for(std::chrono::microseconds(TIME_BETWEEN_ALLOCATIONS));
-        char * memblock = (char *) malloc(pagesize);
+        void * memblock = mmap(NULL, pagesize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
         if (memblock)
             memset(memblock, 0, pagesize);
     }
